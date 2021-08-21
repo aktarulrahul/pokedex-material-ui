fe67026b0a063e122484ad7ab3a64f3bf068b640
aktarulrahul <contact.aktarul@gmail.com>
2021-08-21 19:45:30 +0600

    hooking up to pokeAPI

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,13 +1,29 @@
-import React, { useState } from "react";
-import mockData from "../mockData";
-import { Typography, Link } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+// import mockData from "../mockData";
+import { Typography, Link, CircularProgress, Button } from "@material-ui/core";
 import { toFirstCharUppercase } from "../constants";
+import axios from "axios";
 
 const Pokemon = (props) => {
-  const { match } = props;
+  const { history, match } = props;
   const { params } = match;
   const { pokemonId } = params;
-  const [pokemon, setPokemon] = useState(mockData[`${pokemonId}`]);
+  const [pokemon, setPokemon] = useState(undefined);
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+      .then(function (response) {
+        const { data } = response;
+        setPokemon(data);
+      })
+      .catch(function (error) {
+        setPokemon(false);
+      });
+  }, [pokemonId]);
+  // 1. pokemon=undefined, that means we're getting the onfo --> return loading progess
+  // 2. pokemon=good data, that means we're gotten the onfo --> return actual info
+  // 3. pokemon=bad data / false, that means we're gotten the onfo --> return pokemon not found
+
   const generatePokemonJSX = () => {
     const { name, id, species, height, weight, types, sprites } = pokemon;
     const fullImageUrl = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
@@ -18,7 +34,7 @@ const Pokemon = (props) => {
           {`${id}.`} {toFirstCharUppercase(name)}
           <img src={front_default} />
         </Typography>
-        <img style={{ width: "300px", height: "300px" }} src={fullImageUrl} />
+        <img style={{ width: "300px", height: "300px" }} src={front_default} />
         <Typography variant="h3">Pokemon Info</Typography>
         <Typography>
           {"species: "}
@@ -35,7 +51,19 @@ const Pokemon = (props) => {
       </>
     );
   };
-  return <>{generatePokemonJSX()}</>;
+  return (
+    <>
+      {pokemon === undefined && <CircularProgress />}
+      {pokemon !== undefined && pokemon && generatePokemonJSX(pokemon)}
+      {pokemon === false && <Typography> Pokemon not found</Typography>}
+
+      {pokemon !== undefined && (
+        <Button variant="contained" onClick={() => history.push("/")}>
+          back to pokedex
+        </Button>
+      )}
+    </>
+  );
 };
 
 export default Pokemon;
